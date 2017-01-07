0ba45f575647670afb6cbdfa3e7f403523fbe2bb
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-07 04:38:59 -0600

    added another method for plural relay. put a loop for the loudness input scale for correct input, minor display changes to make output look better

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,3 +1,10 @@
+# Method to print an s if variable is more than 1
+def plural(x)
+    if x > 1
+        print "s"
+    end
+end
+
 def hamster_form
 # Ask user for name of hamster
     puts "What's your hamster's name?"
@@ -6,8 +13,17 @@ def hamster_form
 
 # Ask user for how loud is hamster
     puts "How loud is your hamster, on a scale from 1 to 10 (with 10 being the highest)?"
-# Input value as integer
-    hamster_vol = gets.chomp.to_i
+# Initialize variable to help with loop input
+    hamster_vol = 100
+# Keeps asking user for input until meets the asked conditions
+    until hamster_vol > 0 && hamster_vol < 11
+        hamster_vol = gets.chomp.to_i
+        # If Input is correct first time then proceed or if incorrect show invalid input
+        if hamster_vol < 0 || hamster_vol > 11
+            puts "Sorry Invalid Input. Try Again."      
+        else          
+        end
+    end
 
 # Ask user for color of hamster
     puts "What color is your hamster?"
@@ -20,41 +36,53 @@ def hamster_form
     hamster_adoption = gets.chomp
 
 # Asks user for age of hamster
-    puts "How old is your hamster?"
-# Input value as integer
-    hamster_age = gets.chomp.to_i
+    puts "How old is your hamster? Years -> Months"
+    print "  How Many Years: "
+    hamster_age_years = gets.chomp.to_i
+    print "  How Many Months: "
+    hamster_age_months = gets.chomp.to_i
 
 # Relay back information to user
     # Shows blank line before Relaying input back to user
     puts " "
     # Displays Hamster Name as inputted
-    puts "Hamster name: #{hamster_name}"
+    puts "Hamster Name: #{hamster_name}"
 
-    # Checks to see if volume input number is valid
-        if hamster_vol > 0 && hamster_vol < 11
-            puts "Hamster volume: #{hamster_vol}"
-        else
-            puts "Sorry, Invalid Input of number for loudness of hamster"
-        end
+    # Displays Hamster Loudness From Scale 1-10 highest
+    puts "Hamster Volume: #{hamster_vol}"
 
-    puts "Hamster color: #{hamster_color}"
+    # Displays the user input for the color of hamster
+    puts "Hamster Color: #{hamster_color}"
 
     # Uses boolean to see if candidate is good or bad by user input... or tells user invalid input
         if hamster_adoption == 'Y' || hamster_adoption == 'y' || hamster_adoption == 'yes' || hamster_adoption == 'Yes'
-            puts "Hamster candidate: Good"
+            puts "Hamster Candidate: Good"
         elsif hamster_adoption == 'N' || hamster_adoption == 'n' || hamster_adoption == 'no' || hamster_adoption == 'No'
-            puts "Hamster candidate: Bad"
+            puts "Hamster Candidate: Bad"
         else
-            puts "Sorry, Invalid Input for candidation if good or bad"
+            puts "Sorry, Invalid Input for Candidation"
         end
 
     # Checks to see if age is valid
-        if hamster_age > 0
-            puts "Hamster age: #{hamster_age}"         
+        if (hamster_age_years  >= 2 && hamster_age_months >= 6) || (hamster_age_years > 3)
+            puts " Are you sure it is a hamster or are you lying?"
+            print "Hamster Age: #{hamster_age_years} Year" 
+            plural(hamster_age_years) 
+            print " and #{hamster_age_months} Month" 
+            plural(hamster_age_months)
+            puts " "
+            puts " ITS SO OLD!?!?!"        
         else
-            puts "Sorry, Invalid Input for age"
+            print "Hamster Age: #{hamster_age_years} Year"
+            plural(hamster_age_years)
+            print " and #{hamster_age_months} Month"
+            plural(hamster_age_months)
         end
 end # End of hamster_form method
 
 # Calls the method
 hamster_form
+
+# Couldve told user invalid input as soon as the user inputs it by using loops
+# Come back to do this later for fun!
+# SIDE NOTE TO SELF
