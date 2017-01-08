762326b36fd2a898a5387e3f4d77719e4def4540
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-08 03:20:10 -0600

    added data collection from asking the user

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+
+print "What is your name? "
+	name = gets.chomp
+print "How old are you? "
+	age = gets.chomp.to_i
+print "What year were you born? "
+	year_born = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread!"
+print "Should we order some for you? (Y or N) "
+	order_garlic_bread = gets.chomp
+print "Would you like to enroll in the company's health insurance? (Y or N)"
+	health_insurance = gets.chomp
+
