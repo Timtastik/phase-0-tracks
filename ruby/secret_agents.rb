e6aabd8c16ef62afcc95ee02ef2a5e15e7944133
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-08 01:27:28 -0600

    added some comments to make code more readable and understandable... still not yet finished

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -22,7 +22,7 @@ def encrypt (word)
 		end
 		counter += 1
 	end
-	puts encryptedresult
+	print encryptedresult
 end
 
 def decrypt (encryptedresult)
@@ -44,14 +44,44 @@ def decrypt (encryptedresult)
 		end
 		counter += 1
 	end
-	puts decryptedword
+	print decryptedword
 end
 
 
-	encrypt("abc")
-	encrypt("zed")
-	decrypt("bcd")
-	decrypt("afe")
-	encrypt("robin hood")
-	decrypt(encrypt("robin hood"))
-	decrypt(encrypt("swordfish"))
+	# encrypt("abc")
+	# puts " "
+	# encrypt("zed")
+	# puts " "
+	# decrypt("bcd")
+	# puts " "
+	# decrypt("afe")
+	# puts " "
+	# encrypt("robin hood")
+	# puts " "
+
+
+# Main Interface !!!
+# Greets the Secret Agent
+puts "Hello Secret Agent!"
+# Asks the Agent if want to Decrypt or Encrypt
+puts "Would you like to Decrypt[D or d] or Encrypt[E or e] a password?"
+	user_selection = gets.chomp
+# Asks the Agent for the password
+print "What is the password: "
+	password = gets.chomp
+
+# Uses boolean to decide whether to Decrypt or Encrypt the inputted password
+	if user_selection == "D" || user_selection == "d"
+		# Calls the Decrypt method to decrypt password
+		decrypt(password)
+		print " is the decryption"
+	elsif user_selection == "E" || user_selection == "e"
+		# Calls the Encrypt method to encrypt password
+		encrypt(password)
+		print " is the encryption"
+	else
+		# Tells the Secret Agent that Invalid input for selection of Decrypt or Encrypt	
+		puts "Sorry, Invalid Input for choosing Decrypt or Encrypt"
+	end
+
+# Ruby program now exits
