d623aa1d2d857396449a71bf5a2bcf49e81c3038
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-09 21:19:22 -0600

    Made simple method with a block code and made an array and a hash

---
+++ ruby/iteration.rb
@@ -0,0 +1,11 @@
+sports = ["Basketball", "Soccer", "Football" , "Volleyball"]
+
+hash = {Cubs => "good", White_Sox => "bad", BlackHawks => "good"}
+
+
+def method
+	puts "Yo Man"
+	yield
+end
+
+method { puts "WASSSUUPPP"}
