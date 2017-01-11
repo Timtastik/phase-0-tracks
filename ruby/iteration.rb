c69cd83729ff1b715e7bd5b41230ef5c0265586d
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-11 06:29:39 -0600

    Added some solutions, but unorganized

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,9 +1,40 @@
-sports = ["Basketball", "Soccer", "Football" , "Volleyball"]
+sports = ["basketball", "soccer", "football" , "volleyball"]
 
-hash = {Cubs => "good", White_Sox => "bad", BlackHawks => "good"}
+teams = {"Cubs" => "good", "White_Sox" => "bad", "BlackHawks" => "good"}
 
+# Before calling blocks for each
+p sports
+
+sports.each do |sport|
+	puts "#{sport} is Fun!"
+end
+
+# After calling blocks for each
+p sports
+
+# Before calling hash for each
+p teams
+
+teams.each do |team, status|
+	puts "#{team} is #{status}"
+end
+
+# After calling hash for each
+p teams
+
+# Before calling blocks for map!
+p sports
+
+sports.map! do |sport|
+	puts sport
+	sport.upcase
+end
+
+# Aafter calling blocks for map!
+p sports
 
 def method
+	puts " "
 	puts "Yo Man"
 	yield
 end
