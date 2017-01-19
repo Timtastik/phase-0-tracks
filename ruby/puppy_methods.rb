1583557af6f0aab3069c1962ad6966eddf88c0ed
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-19 13:04:44 -0600

    Added driver code and initialized class instance and added more methods to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(numb)
+  	n = 0
+  	while n < numb
+  		print "Woof! "
+  	n += 1
+  	end
+  	puts ""
+  end
+
+  def roll_over()
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_year)
+  	dog_year = human_year / 7
+  	puts dog_year
+  end
+
+  def jump()
+  	puts "*JUMPY*"
+  end
+
 end
 
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(21)
+puppy.jump
