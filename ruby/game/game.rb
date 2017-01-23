4353cb6fe220dc7a24cda4f7b95f7c3d26883102
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-23 09:39:47 -0600

    refactoring and debugging code in game.rb and started testing the instance methods in game_spec.rb

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,67 +1,112 @@
+class Game
+	attr_reader :array_word_input, :feedback, :guess_count, :array_of_guesses
 
+	def initialize
+		@array_of_guesses = []
+		@guess_count = 0
+	end
 
+	def relay_feedback()
+		@feedback.each { |letter| print "#{letter} "}
+	end
 
+	def winner()
+		puts "CONGRATZ! YOU'VE GUESSED CORRECTLY!"
+	end
 
+	def loser()
+		
+	end
 
+	def word_to_array(string_word)
+		@array_word_input = string_word.split('')
+	end
 
+	def array_feedback(string_word)
+		@feedback = Array.new(string_word.length, "_")
+	end
 
+	def wrong_guess()
+		puts "Sorry! Your guess is wrong!"
+	end
 
+	def same_guess()
+		puts "Already guessed that before!"
+	end
 
+	def new_guess(inputted_guess)
+		@array_of_guesses << inputted_guess
+	end
 
+	def invalid_input()
+		puts "Invalid Input. YOU LOSE A GUESS!"
+	end
+
+	def guess_increment()
+		@guess_count += 1
+	end
+end
+
+game = Game.new
 
 puts "Please enter a word"
 word_input = gets.chomp
-array_word_input = word_input.split('')
-feedback = Array.new(word_input.length, "_")
+# array_word_input = word_input.split('')
+game.word_to_array(word_input)
+# feedback = Array.new(word_input.length, "_")
+game.array_feedback(word_input)
+# array_of_guesses = []
 
-array_of_guesses = []
-
-guess = nil
-n = 0
-while n < word_input.length
+game.guess_count
+while game.guess_count < word_input.length
 
 	puts "Please enter guess"
 	guess = gets.chomp
 
 	i = 0
 	while i < array_of_guesses.length
-		if array_of_guesses[i] == guess
-			puts "Already guessed that before!"
+		if game.array_of_guesses[i] == guess
+			game.same_guess
 			break
 		else
-			array_of_guesses.push(guess)
+			game.new_guess(guess)
+			p game.array_of_guesses
 		end
 		i += 1
 	end
 
 	if guess.length == 1
-		x = 0
 		if guess == word_input
-			puts "CONGRATZ! YOU'VE GUESSED CORRECTLY!"
+			game.winner
 			break
 		end
+		x = 0
 		while x < word_input.length
-			if array_word_input[x] == guess
-					feedback[x] = guess
+			if game.array_word_input[x] == guess
+					game.feedback[x] = guess
 			end	
 			x += 1
 		end
-		feedback.each {|letter| print "#{letter} "}
+		game.relay_feedback
 		puts ""
 		if array_of_guesses.include?(guess) == false
-			n += 1
+			game.guess_increment
+			p game.array_of_guesses
 		end
+
 	elsif guess.length > 1
 		if guess == word_input
-			puts "CONGRATZ! YOU'VE GUESSED CORRECTLY!"
+			game.winner
 			break
 		else
-			puts "Sorry! Your guess is wrong!"
-			n += 1
+			game.wrong_guess
+			game.relay_feedback
+			game.guess_increment
 		end
+
 	else
-		puts "Invalid Input. YOU LOSE A GUESS!"
-		n += 1
+		game.invalid_input
+		game.guess_increment
 	end
 end
 
---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'game'
+
+describe Game do
+	let (:game) { Game.new }
+
+	it "initialize array of guesses to be blank array" do
+		expect(game.array_of_guesses).to eq []
+	end
+
+	it "pushes new element into array of guesses" do
+		expect(game.new_guess("lol")).to eq ["lol"]
+	end
+
+	it "show the first element of array of guesses" do
+		expect(game.array_of_guesses[0]).to eq nil
+	end
+
+	it "pushes new element and checks the element array of guesses" do
+		game.new_guess("lol")
+		expect(game.array_of_guesses[0]).to eq "lol"
+	end
+end
