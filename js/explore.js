fb6c1c510d9bb3c48098d647bd9c56587358120d
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-29 19:37:46 -0600

    answered the question, but looking to refactor now

--- js/explore.js
+++ js/explore.js
@@ -7,3 +7,24 @@ PSUEDO CODE
 - The function will return the backwards string 
 */
 
+function reverse_string(input_string){
+	var array_of_string = [];
+	var backwards_string = [];
+
+	for (var i = 0; i < input_string.length; i++) {
+		array_of_string += input_string[i];
+	};
+
+	console.log(array_of_string);
+
+	for (var i = array_of_string.length - 1; i >= 0; i--) {
+		backwards_string += array_of_string[i]
+	};
+
+	console.log(backwards_string)
+
+};
+
+// DRIVER TEST CODE
+
+reverse_string("hello");
