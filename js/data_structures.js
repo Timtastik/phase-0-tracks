87a15fbb967dbe54849c7f1b5565f37676c21a28
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-29 23:40:51 -0600

    made an array of colors and an array of names... pushed another color into array and another name into another array

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colors = ["green", "red", "blue", "yellow"];
+var names = ["Ed", "Sea Biscuit", "Joe", "Speedster"];
+
+colors.push("magenta");
+names.push("Tornado");
+console.log(colors);
+console.log(names);
