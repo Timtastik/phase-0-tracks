946706f064f9910642eddda406200abaa7436032
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-30 00:03:56 -0600

    added an object horse with properties of names and values of colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,13 @@ colors.push("magenta");
 names.push("Tornado");
 console.log(colors);
 console.log(names);
+
+var horse = {};
+
+if (names.length == colors.length) {
+	for (var i = 0; i < names.length; i++) {
+		horse[names[i]] = colors[i];
+	};
+};
+
+console.log(horse);
