fd044380e64c76df59b1a7e67026a3606bc1524f
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-29 19:21:15 -0600

    added explore javascrit file with psuedo code

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+/*
+PSUEDO CODE
+- A function that takes in a string as a parameter
+- The function will store the characters of the string into an array
+- The function will loop to make last charcters in first
+-- and front characters last
+- The function will return the backwards string 
+*/
+
