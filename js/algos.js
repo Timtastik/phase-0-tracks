2fd457aab59f08433084ecf82bf64b6178b83bbe
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-30 01:44:01 -0600

    added pseudo code to think of solving the problem

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// DO THE THING 1
+// PSEUDO CODE
+// Have a bunch of strings in an array
+// Iterate through the array of strings to see which has longest length
+// If string is the longest then store that into a variable
+// Keep looping until checks all the strings of the array
+// Case if more than one string is the longest and same length?
