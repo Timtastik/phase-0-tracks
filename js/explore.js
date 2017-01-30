a565fe1a65a9d4cf13ec9fb244544eb790065943
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-29 19:44:14 -0600

    refactor the code to look more simple... still needs polishing

--- js/explore.js
+++ js/explore.js
@@ -8,23 +8,16 @@ PSUEDO CODE
 */
 
 function reverse_string(input_string){
-	var array_of_string = [];
-	var backwards_string = [];
+	var backwards_string = "";
 
-	for (var i = 0; i < input_string.length; i++) {
-		array_of_string += input_string[i];
+	for (var i = input_string.length - 1; i >= 0; i--) {
+		backwards_string += input_string[i]
 	};
 
-	console.log(array_of_string);
-
-	for (var i = array_of_string.length - 1; i >= 0; i--) {
-		backwards_string += array_of_string[i]
-	};
-
-	console.log(backwards_string)
+	return backwards_string
 
 };
 
 // DRIVER TEST CODE
 
-reverse_string("hello");
+console.log(reverse_string("hello"));
