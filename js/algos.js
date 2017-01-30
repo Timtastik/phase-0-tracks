2efeeb7f8e980181317d603d8c54783b486c53ec
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-01-30 01:44:30 -0600

    added code that I thought would work, but doesn't actually

--- js/algos.js
+++ js/algos.js
@@ -5,3 +5,20 @@
 // If string is the longest then store that into a variable
 // Keep looping until checks all the strings of the array
 // Case if more than one string is the longest and same length?
+
+var strings = ["long", "short", "something", "absolutely the longest one"];
+
+function longest_string(array_of_strings){
+	var long_string = "";
+
+	for(var i = 0; i < array_of_strings.length; i++){
+		if (long_string < array_of_strings[i].length) {
+			long_string = array_of_strings[i];
+		}; // if
+	}; // for
+
+	return long_string;
+
+}; // function
+
+console.log(longest_string(strings));
