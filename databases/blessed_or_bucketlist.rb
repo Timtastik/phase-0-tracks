d8c953cc329b865c3e5c029c3e91612531284fd6
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-02-06 08:23:38 -0600

    added more to the creation of the tables with primary keys and foreign keys

--- databases/blessed_or_bucketlist.rb
+++ databases/blessed_or_bucketlist.rb
@@ -1,5 +1,7 @@
 require 'sqlite3'
 
+
+
 # def add_to_blessings(db, comment)
 # 	db.execute("INSERT INTO blessings (comment) VALUES (?)", [comment])
 # end
@@ -24,6 +26,10 @@ def update(db, table, column, new_comment, where_id)
 	db.execute("UPDATE #{table} SET #{column}='#{new_comment}' WHERE id='where_id'")
 end
 
+def view_blessings(db,)
+	db.execute("SELECT * FROM blessings")
+end
+
 puts "To access your Blessed and Buckets database"
 puts "Please enter your username!"
 puts "If you do not have a username this is also the time to create one"
@@ -32,26 +38,40 @@ username = gets.chomp
 
 database = SQLite3::Database.new("#{username}.db")
 
+current_date = database.execute("SELECT date('now')")
+
 create_user_table = <<-SQL
 	CREATE TABLE user(
 		id INTEGER PRIMARY KEY,
 		username VARCHAR(30),
 		first_name VARCHAR(20),
-		last_name VARCHAR(20)
+		last_name VARCHAR(20),
+		blessings_id INT,
+		buckets_id INT,
+		FOREIGN KEY (blessings_id) REFERENCES blessings(id),
+		FOREIGN KEY (buckets_id) REFERENCES buckets(id)
 		);
 SQL
 
 create_blessings_table = <<-SQL
 	CREATE TABLE blessings(
 		id INTEGER PRIMARY KEY,
-		list_blessing VARCHAR(255)
+		comment VARCHAR(255),
+		date_entered DATE,
+		stars INT
 		);
 SQL
 
 create_bucket_list_table = <<-SQL
 	CREATE TABLE buckets(
 		id INTEGER PRIMARY KEY,
-		list_bucket VARCHAR(255)
+		comment VARCHAR(255)
+		date_entered DATE,
+		stars INT
 		);
 SQL
+
+database.execute(create_user_table)
+database.execute(create_blessings_table)
+database.execute(create_bucket_list_table)
 
