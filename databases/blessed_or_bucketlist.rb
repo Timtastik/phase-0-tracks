7636c4fab7e12c300de2ac86d57e5a463ec32f99
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-02-06 11:23:40 -0600

    refactored methods to update the database, but not recommended to update

--- databases/blessed_or_bucketlist.rb
+++ databases/blessed_or_bucketlist.rb
@@ -1,11 +1,22 @@
+# PSEUDO CODE
+# This is a program that has one username for each database
+# Within this database there are two tables of a blessings lists and bucket lists
+# Blessings are what a user considers a good thing and adds it to this list
+# Buckets are what a user consider wants a good thing to happen to them
+# User can add to either list
+# Usually Blessings are occuring right now or mainly in the past
+# Usually Buckets are in the future and hope that will happen
+# One awesome idea is that when confirmed a bucket item has been achieved then transfers to the blessing list
+# Right now this is in the works
+
 require 'sqlite3'
 
 def add_to_blessings(db, comment, rating)
-	db.execute("INSERT INTO blessings (bless, stars, date_entered) VALUES (?, ?, ?)", [comment, rating, current_date])
+	db.execute("INSERT INTO blessings (bless, stars) VALUES (?, ?)", [comment, rating])
 end
 
 def add_to_buckets(db, comment, rating)
-	db.execute("INSERT INTO buckets (bucket, stars, date_entered) VALUES (?, ?, ?)", [comment, rating, current_date])
+	db.execute("INSERT INTO buckets (bucket, stars) VALUES (?, ?)", [comment, rating])
 end
 
 # def update_blessings(db, new_comment, where_id)
@@ -16,16 +27,28 @@ end
 # 	db.execute("UPDATE buckets SET comment='#{new_comment}' WHERE id='where_id'")
 # end
 
-def update(db, table, column, new_comment, where_id)
-	db.execute("UPDATE #{table} SET #{column}='#{new_comment}' WHERE id='where_id'")
+def update_buckets(db, table, new_comment, where_id)
+	db.execute("UPDATE #{table} SET bucket='#{new_comment}' WHERE id='where_id'")
+end
+
+def update_blessings(db, table, new_comment, where_id)
+	db.execute("UPDATE #{table} SET bless='#{new_comment}' WHERE id='where_id'")
 end
 
 def view_blessings(db)
-	db.execute("SELECT bless, stars, date_entered FROM blessings")
+	blessings_list = db.execute("SELECT bless, stars FROM blessings")
+	puts "BLESSINGS LIST"
+	blessings_list.each do |blessing|
+		puts "Comment:#{blessing['bless']}__Stars:#{blessing['stars']}"
+	end
 end
 
 def view_buckets(db)
-	db.execute("SELECT bucket, stars, date_entered FROM buckets")
+	buckets_list = db.execute("SELECT bucket, stars, date_entered FROM buckets")
+	puts "BUCKET LIST"
+	buckets_list.each do |buck|
+		puts "#{buck['bucket']}__Stars:#{bucket['stars']}"
+	end
 end
 
 def view_both(db)
@@ -36,42 +59,30 @@ def view_both(db)
 		INNER JOIN buckets ON user.buckets_id = buckets.id
 	SQL
 
-	db.execute(viewing_both_tables)
-end
-
-def who_am_i(db)
-	db.execute("SELECT username, first_name, last_name FROM user")
+	both_tables = db.execute(viewing_both_tables)
+	puts "BLESSINGS AND BUCKETS"
+	p both_tables
 end
 
 def menu
-	puts "What would you like to do?"
-	puts "ADD to your blessings/buckets list?"
-	puts "UPDATE to your blessings/buckets list?"
-	puts "VIEW one or more lists?"
-	puts "DELETE an item from a list?"
-	puts "SEE menu again?"
-	puts "or you're 'DONE' here?"
+	puts ""
+	puts "  What would you like to do?"
+	puts "  ADD to your blessings/buckets list?"
+	puts "  UPDATE to your blessings/buckets list?"
+	puts "  VIEW one or more lists?"
+	puts "  DELETE an item from a list?"
+	puts "  or you're 'DONE' here?"
 end
 
-puts "To access your Blessed and Buckets database"
-puts "Please enter your username!"
-puts "If you do not have a username this is also the time to create one"
-puts "What is your user name?"
+puts "This is the Blessings and Buckets program"
+puts "Please enter your username or create a new one!"
 username = gets.chomp
 
-
 database = SQLite3::Database.new("#{username}.db")
-
-
-
-$current_date = database.execute("SELECT date('now')")
+database.results_as_hash = true
 
 create_user_table = <<-SQL
-	CREATE TABLE user(
-		id INTEGER PRIMARY KEY,
-		username VARCHAR(30),
-		first_name VARCHAR(20),
-		last_name VARCHAR(20),
+	CREATE TABLE IF NOT EXISTS user(
 		blessings_id INT,
 		buckets_id INT,
 		FOREIGN KEY (blessings_id) REFERENCES blessings(id),
@@ -80,19 +91,17 @@ create_user_table = <<-SQL
 SQL
 
 create_blessings_table = <<-SQL
-	CREATE TABLE blessings(
+	CREATE TABLE IF NOT EXISTS blessings(
 		id INTEGER PRIMARY KEY,
 		bless VARCHAR(255),
-		date_entered DATE,
 		stars INT
 		)
 SQL
 
 create_bucket_list_table = <<-SQL
-	CREATE TABLE buckets(
+	CREATE TABLE IF NOT EXISTS buckets(
 		id INTEGER PRIMARY KEY,
-		bucket VARCHAR(255)
-		date_entered DATE,
+		bucket VARCHAR(255),
 		stars INT
 		)
 SQL
@@ -101,10 +110,11 @@ database.execute(create_user_table)
 database.execute(create_blessings_table)
 database.execute(create_bucket_list_table)
 
-menu
+# view_both(database)
 
 user_input = ""
 while user_input != 'DONE'
+	menu
 
 	user_input = gets.chomp.upcase
 
@@ -130,7 +140,28 @@ while user_input != 'DONE'
 			add_to_buckets(database, comment, rate)
 		end
 	elsif user_input == "UPDATE"
-		
+		puts "Best to update only if Typo... "
+		puts "Which table? (blessings) or (buckets)?"
+		table_wanted = gets.chomp
+
+		if table_wanted == "blessings"
+			puts "For which ID identifier number?"
+			identifier = gets.chomp.to_i
+
+			puts "What is the comment?"
+			comment = gets.chomp
+
+			update_blessings(database, table_wanted, comment, identifier)
+
+		elsif table_wanted == "buckets"
+			puts "For which ID identifier number?"
+			identifier = gets.chomp.to_i
+
+			puts "What is the comment?"
+			comment = gets.chomp
+
+			update_buckets(database, table_wanted, comment, identifier)
+		end
 	elsif user_input == "VIEW"
 		puts "Which table would you like to view?"
 		puts "Your bucket list[1], blessings list[2], or both[3]?"
@@ -138,15 +169,13 @@ while user_input != 'DONE'
 		table_wanted = gets.chomp.to_i
 	
 		if table_wanted == 1
-			view_buckets
+			view_buckets(database)
 		elsif table_wanted == 2
-			view_blessings
+			view_blessings(database)
 		elsif table_wanted == 3
-			view_both
+			view_both(database)
 		end
 	elsif user_input == "DELETE"
 
-	elsif user_input == "SEE"
-		menu
 	end
 end
