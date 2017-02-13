f891aa30288ac9506f5365e34eed5dff05f46a50
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-02-13 00:25:44 -0600

    worked with Chase Stephens, copied and pasted the code we worked together from his directory

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -2,6 +2,7 @@
 require 'sinatra'
 require 'sqlite3'
 
+# add static resources
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
@@ -24,4 +25,32 @@ post '/students' do
   redirect '/'
 end
 
-# add static resources
+# student location counter
+get "/students/count" do 
+  sql_loc = <<-SQL
+  SELECT COUNT(name), campus FROM students GROUP BY campus HAVING campus="SF" 
+  UNION 
+  SELECT COUNT(name), campus FROM students GROUP BY campus HAVING campus="SD"
+  UNION 
+  SELECT COUNT(name), campus FROM students GROUP BY campus HAVING campus="CHI"
+  UNION 
+  SELECT COUNT(name), campus FROM students GROUP BY campus HAVING campus="SEA"
+  UNION 
+  SELECT COUNT(name), campus FROM students GROUP BY campus HAVING campus="NYC"
+  SQL
+  @students_loc = db.execute(sql_loc)
+  erb :students_locations
+end
+
+# birthday for a studnet
+# update database with +1 on age
+get '/students/birthday' do
+  erb :birthday
+end
+
+post '/birthday' do
+  cur_age = db.execute("SELECT age FROM students WHERE name=?",[params['name']])
+  new_age = cur_age[0]['age'].to_i + 1
+  db.execute("UPDATE students SET age=? WHERE name=?", [new_age,params['name']])
+  redirect '/'
+end
