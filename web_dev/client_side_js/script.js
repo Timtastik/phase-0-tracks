553b6038d8392cc5e1d0ac5cb04bcb69f63fc25d
Timothy I. Villeroz <timothy.villeroz@gmail.com>
2017-02-12 23:37:23 -0600

    added more features with classes and finalize a code for submission

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -6,22 +6,41 @@ function hover_out(){
 	event.target.style.backgroundColor="white";
 };
 
-function bigger(){
-	event.target.style.fontSize="20em";
+function hello(){
+	alert("Hello there!");
 };
 
 function smaller(){
 	event.target.style.fontSize=".5em";
 };
 
+function insert_new(){
+	var new_header = document.createElement("h4");
+	var node = document.createTextNode("HOPE YOU HAVE A GREAT DAY!!!");
+	new_header.appendChild(node);
+
+	var el = document.getElementById("editing");
+	var kid = document.getElementById("alright");
+	el.insertBefore(new_header, kid);
+};
+
+function turn_red(){
+	event.target.style.color="red";
+};
+
 var tom = document.getElementById("tom");
 var title = document.getElementById("title");
-var stored = document.getElementById("secondhalf");
+var tim = document.getElementById("tim");
+var red = document.getElementsByClassName("red");
 
 tom.addEventListener("mouseover", hover);
 tom.addEventListener("mouseout", hover_out);
-title.addEventListener("resize", bigger);
-// stored.addEventListener("click", smaller);
-// parent.addEventListener("mouseover", hovering);
+title.addEventListener("click", hello);
+tim.addEventListener("click", smaller);
+insert_new();
+
+for(var i = 0; i < red.length; i++){
+	red[i].addEventListener("mouseover", turn_red);
+};
 
 console.log("script is running");
